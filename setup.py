a10c15ffdc59d5f2b36dd4f84f208150964c692d
Anonymous User <ksshah55@gmail.com>
2026-04-07 12:17:36 -0400

    Checkin v1 of the version

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name='docassemble.CertificateOfService',
       license='MIT',
       url='https://courtformsonline.org',
       packages=find_namespace_packages(),
-      install_requires=['docassemble.ALMassachusetts>=0.1.2', 'docassemble.AssemblyLine @ git+https://github.com/SuffolkLITLab/docassemble-AssemblyLine.git@main', 'docassemble.MassAccess @ git+https://github.com/SuffolkLITLab/docassemble-MassAccess.git@main'],
+      install_requires=['docassemble.ALMassachusetts>=0.2.0', 'docassemble.AssemblyLine @ git+https://github.com/SuffolkLITLab/docassemble-AssemblyLine.git@main', 'docassemble.MassAccess @ git+https://github.com/SuffolkLITLab/docassemble-MassAccess.git@main'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/CertificateOfService/', package='docassemble.CertificateOfService'),
      )
